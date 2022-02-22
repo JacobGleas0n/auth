7168bda5a961ff30959c436a7be763f8e4e4d215
JacobGleas0n <jacob.gleason@kellogg.northwestern.edu>
2022-02-22 15:38:48 +0000

    first encryption

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params["user"])
+  #params["user"]=>
+  # {"first_name"=>"Jacob", 
+  #"last_name"=>"Gleason", 
+  #"email"=>"jacob.gleason@kellogg.northwestern.edu", 
+  #"password"=>"tacos"}
+  plain_text_password = params["user"]["password"] #=> tacos   
+  @user = User.new(params["user"])
+    @user.password = BCrypt::Password.create(plain_text_password) 
     @user.save
     redirect_to "/users/#{@user.id}"
   end
--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -6,3 +6,13 @@
 # 1. encrypt a secret and puts encrypted string
 # 2. decrypt the encrypted string
 # 3. check if decrypted value matches secret
+
+secret_password = "tacos"
+encrypted_password = BCrypt::Password.create(secret_password)
+puts encrypted_password
+
+#password_to_test = "tacos" would return true
+password_to_test = "sushi"
+#returns false because sushi isn't equal to the real password
+password_matches = BCrypt::Password.new(encrypted_password) == password_to_test
+puts password_matches
