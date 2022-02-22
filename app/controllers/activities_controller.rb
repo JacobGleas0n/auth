e4e668802aec0a45df54d8c83b5665bdaa4d67cc
JacobGleas0n <jacob.gleason@kellogg.northwestern.edu>
2022-02-22 17:08:35 +0000

    need to fix user id

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,11 @@
 class ActivitiesController < ApplicationController
+  
   def create
     @activity = Activity.new(params["activity"])
+    @activity.user_id = @current_user.user_id
     @activity.save
     redirect_to "/contacts/#{@activity.contact_id}"
   end
+
+
 end
