d42621e7fc7bdf69cdb3be4b64e0d224f9162e0e
JacobGleas0n <jacob.gleason@kellogg.northwestern.edu>
2022-02-22 17:30:41 +0000

    completed

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@ class ActivitiesController < ApplicationController
   
   def create
     @activity = Activity.new(params["activity"])
-    @activity.user_id = @current_user.user_id
+    @activity.user_id = @current_user.id
     @activity.save
     redirect_to "/contacts/#{@activity.contact_id}"
   end
